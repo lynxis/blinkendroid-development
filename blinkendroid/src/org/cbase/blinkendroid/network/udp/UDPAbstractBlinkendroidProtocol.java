f1abc49628680049b546c93a85cfa4e92b93184f
lischke <lischke@ded441ff-28c8-dbd9-35dd-4b5d9abe79e0>
2010-10-13 21:15:11 +0000

    fixed heartbeat
    git-svn-id: http://blinkendroid.googlecode.com/svn/branches/development@488 ded441ff-28c8-dbd9-35dd-4b5d9abe79e0

--- blinkendroid/src/org/cbase/blinkendroid/network/udp/UDPAbstractBlinkendroidProtocol.java
+++ blinkendroid/src/org/cbase/blinkendroid/network/udp/UDPAbstractBlinkendroidProtocol.java
@@ -20,131 +20,136 @@ import android.util.Log;
 
 public class UDPAbstractBlinkendroidProtocol implements UDPDirectConnection {
 
-  protected BufferedOutputStream out;
-  protected BufferedInputStream in;
-  protected DatagramSocket m_Socket;
-  protected ReceiverThread receiverThread;
-  protected final HashMap<Integer, CommandHandler> handlers = new HashMap<Integer, CommandHandler>();
-  protected List<ConnectionListener> connectionListener = new ArrayList<ConnectionListener>();
-  protected boolean server;
+    protected BufferedOutputStream out;
+    protected BufferedInputStream in;
+    protected DatagramSocket m_Socket;
+    protected ReceiverThread receiverThread;
+    protected final HashMap<Integer, CommandHandler> handlers = new HashMap<Integer, CommandHandler>();
+    protected List<ConnectionListener> connectionListener = new ArrayList<ConnectionListener>();
+    protected boolean server;
 
-  protected UDPAbstractBlinkendroidProtocol(final DatagramSocket socket) throws IOException {
+    protected UDPAbstractBlinkendroidProtocol(final DatagramSocket socket) throws IOException {
 	this.m_Socket = socket;
 	receiverThread = new ReceiverThread();
 	receiverThread.start();
-  }
+    }
 
-  public void addConnectionListener(ConnectionListener connectionListener) {
+    public void addConnectionListener(ConnectionListener connectionListener) {
 	this.connectionListener.add(connectionListener);
-  }
+    }
 
-  public void registerHandler(Integer proto, CommandHandler handler) {
+    public void registerHandler(Integer proto, CommandHandler handler) {
 	handlers.put(proto, handler);
-  }
+    }
 
-  public void unregisterHandler(CommandHandler handler) {
+    public void unregisterHandler(CommandHandler handler) {
 	handlers.remove(handler);
-  }
+    }
 
-  public void close() {
+    public void close() {
 	try {
-	  out.close();
-	  if (!server)// TODO ugly hack, server needs to long
+	    out.close();
+	    if (!server)// TODO ugly hack, server needs to long
 		in.close();
-	  m_Socket.close();
-	  Log.d(Constants.LOG_TAG, getMyName() + " BlinkendroidProtocol: Socket closed.");
+	    m_Socket.close();
+	    Log.d(Constants.LOG_TAG, getMyName() + " BlinkendroidProtocol: Socket closed.");
 	} catch (IOException e) {
-	  Log.e(Constants.LOG_TAG, getMyName() + " BlinkendroidProtocol: closed failed ");
+	    Log.e(Constants.LOG_TAG, getMyName() + " BlinkendroidProtocol: closed failed ");
 	}
-  }
+    }
 
-  public void shutdown() {
+    public void shutdown() {
 	if (null != receiverThread) {
-	  receiverThread.shutdown();
+	    receiverThread.shutdown();
 	}
 	handlers.clear();
 	Log.d(Constants.LOG_TAG, getMyName() + " Protocol shutdown.");
 	// close();
-  }
+    }
 
-  protected void receive(DatagramPacket packet) throws IOException {
+    protected void receive(DatagramPacket packet) throws IOException {
 
 	InetSocketAddress socketAddress = (InetSocketAddress) packet.getSocketAddress();
 	ByteBuffer in = ByteBuffer.wrap(packet.getData());
 	int proto = in.getInt();
+	int pos = in.position();
 	Log.d(Constants.LOG_TAG, "BlinkendroidClient received Protocol: " + proto);
 	if (proto == Constants.PROTOCOL_HEARTBEAT) {
-	  for (CommandHandler h : handlers.values()) {
+	    for (CommandHandler h : handlers.values()) {
+		// Log.d(Constants.LOG_TAG,
+		// "BlinkendroidClient get protocl heartrbeat for handler"
+		// + h.getClass().getName());
 		h.handle(socketAddress, in);
-	  }
-	  return;
+		in.position(pos);
+	    }
+	    return;
 	}
 
 	CommandHandler handler = handlers.get(proto);
-	System.out.println("recieve proto "+proto+" handler "+handler.getClass().toString());
+	System.out.println("recieve proto " + proto + " handler " + handler.getClass().toString());
 	if (null != handler)
-	  handler.handle(socketAddress, in);
-  }
+	    handler.handle(socketAddress, in);
+    }
 
-  // Inner classes:
-  /**
-   * A thread that receives information
-   */
-  class ReceiverThread extends Thread {
+    // Inner classes:
+    /**
+     * A thread that receives information
+     */
+    class ReceiverThread extends Thread {
 
 	volatile private boolean running = true;
 
 	@Override
 	public void run() {
-	  this.setName("--- ReceiverThread");
-	  running = true;
-	  Log.d(Constants.LOG_TAG, "InputThread started");
-	  byte[] receiveData;
-	  DatagramPacket receivePacket;
-	  try {
+	    this.setName("--- ReceiverThread");
+	    running = true;
+	    Log.d(Constants.LOG_TAG, "InputThread started");
+	    byte[] receiveData;
+	    DatagramPacket receivePacket;
+	    try {
 		m_Socket.setSoTimeout(1000);
 		while (running) {
-		  receiveData = new byte[1024];
-		  receivePacket = new DatagramPacket(receiveData, receiveData.length);
-		  Log.d(Constants.LOG_TAG, this.getName() + " received " + receivePacket.toString());
-		  try {
+		    receiveData = new byte[1024];
+		    receivePacket = new DatagramPacket(receiveData, receiveData.length);
+		    Log.d(Constants.LOG_TAG, this.getName() + " received " + receivePacket.toString());
+		    try {
 			m_Socket.receive(receivePacket);
 			receive(receivePacket);
-		  } catch (InterruptedIOException e) {
+		    } catch (InterruptedIOException e) {
 			// timeout happened - just a normal case, swallowing
-//			Log.i(Constants.LOG_TAG, "ReceiverThread timeout");
-		  }
+			// Log.i(Constants.LOG_TAG, "ReceiverThread timeout");
+		    }
 		}
-	  } catch (SocketException e) {
+	    } catch (SocketException e) {
 		Log.e(Constants.LOG_TAG, "InputThread Socket closed", e);
-	  } catch (IOException e) {
+	    } catch (IOException e) {
 		Log.e(Constants.LOG_TAG, "InputThread IOException", e);
 		e.printStackTrace();
-	  }
+	    }
 	}
 
 	public void shutdown() {
-	  Log.d(Constants.LOG_TAG, getMyName() + " ReceiverThread shutdown start");
-	  running = false;
-	  interrupt();
-	  Log.d(Constants.LOG_TAG, getMyName() + " ReceiverThread shutdown interrupted");
-	  try {
+	    Log.d(Constants.LOG_TAG, getMyName() + " ReceiverThread shutdown start");
+	    running = false;
+	    interrupt();
+	    Log.d(Constants.LOG_TAG, getMyName() + " ReceiverThread shutdown interrupted");
+	    try {
 		join();
-	  } catch (InterruptedException e) {
+	    } catch (InterruptedException e) {
 		Log.e(Constants.LOG_TAG, getMyName() + " ReceiverThread join failed", e);
-	  }
-	  Log.d(Constants.LOG_TAG, getMyName() + " ReceiverThread shutdown joined & end");
+	    }
+	    Log.d(Constants.LOG_TAG, getMyName() + " ReceiverThread shutdown joined & end");
 	}
-  }
+    }
 
-  public void send(InetSocketAddress socketAddr, ByteBuffer out) throws IOException {
+    public void send(InetSocketAddress socketAddr, ByteBuffer out) throws IOException {
 	m_Socket.send(new DatagramPacket(out.array(), out.position(), socketAddr));
-  }
+    }
 
-  protected String getMyName() {
+    protected String getMyName() {
 	if (server)
-	  return "Server ";// + socket.getRemoteSocketAddress();
+	    return "Server ";// + socket.getRemoteSocketAddress();
 	else
-	  return "Client ";// + socket.getRemoteSocketAddress();
-  }
+	    return "Client ";// + socket.getRemoteSocketAddress();
+    }
 }
