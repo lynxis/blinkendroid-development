51762ebb2de0e9e86c8b967fb024ba6c20a8de06
lischke <lischke@ded441ff-28c8-dbd9-35dd-4b5d9abe79e0>
2010-10-17 18:01:23 +0000

    ticketmanager always sent tickets
    git-svn-id: http://blinkendroid.googlecode.com/svn/branches/development@499 ded441ff-28c8-dbd9-35dd-4b5d9abe79e0

--- blinkendroid/src/org/cbase/blinkendroid/server/TicketManager.java
+++ blinkendroid/src/org/cbase/blinkendroid/server/TicketManager.java
@@ -3,6 +3,7 @@ package org.cbase.blinkendroid.server;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
+import java.net.SocketException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,64 +15,72 @@ import org.cbase.blinkendroid.network.udp.ClientSocket;
 import android.util.Log;
 
 public class TicketManager implements IPeerHandler, ConnectionListener {
-  int maxClients = 2;
-  int clients = 0;
-  private Set<String> tickets = new HashSet<String>();
-  private String ownerName;
+    int maxClients = 2;
+    int clients = 0;
+    private Set<String> tickets = new HashSet<String>();
+    private String ownerName;
+    DatagramSocket socket = null;
 
-  public TicketManager(String ownerName) {
+    public TicketManager(String ownerName) {
 	this.ownerName = ownerName;
-  }
-
-  public void foundPeer(String name, String ip, int protocolVersion) {
-	System.out.printf("foundpeer " + name + ip);
-	if (tickets.contains(name + ip)) {
-	  Log.d(Constants.LOG_TAG, "already sent ticket for " + name + " " + ip);
-	  return;
+	try {
+	    socket = new DatagramSocket(Constants.BROADCAST_ANNOUCEMENT_SERVER_TICKET_PORT);
+	    socket.setReuseAddress(true);
+	} catch (SocketException e) {
+	    System.out.println("new DatagramSocket(Constants.BROADCAST_ANNOUCEMENT_SERVER_TICKET_PORT) failed "
+		    + e.getMessage());
 	}
+    }
+
+    public void foundPeer(String name, String ip, int protocolVersion) {
 	// noch platz frei?
-	if (clients < maxClients) {
-	  // send ticket to ip
-	  try {
-		InetSocketAddress socketAddr = new InetSocketAddress(ip, Constants.BROADCAST_ANNOUCEMENT_CLIENT_TICKET_PORT);
-		String message = Constants.BROADCAST_PROTOCOL_VERSION + " " + Constants.SERVER_TICKET_COMMAND + " " + ownerName;
+	if (clients < maxClients || tickets.contains(ip)) {
+	    // send ticket to ip
+	    try {
+		InetSocketAddress socketAddr = new InetSocketAddress(ip,
+			Constants.BROADCAST_ANNOUCEMENT_CLIENT_TICKET_PORT);
+		String message = Constants.BROADCAST_PROTOCOL_VERSION + " " + Constants.SERVER_TICKET_COMMAND + " "
+			+ ownerName;
 		final byte[] messageBytes = message.getBytes("UTF-8");
 		final DatagramPacket packet = new DatagramPacket(messageBytes, messageBytes.length, socketAddr);
-		System.out.printf("UDP SOCKET CREATED");
-		DatagramSocket socket = new DatagramSocket(Constants.BROADCAST_ANNOUCEMENT_SERVER_TICKET_PORT);
-		socket.setReuseAddress(true);
 		socket.send(packet);
-		Log.d(Constants.LOG_TAG, "send Ticket");
-		clients++;
-		tickets.add(name + ip);
-	  } catch (Exception e) {
+		Log.d(Constants.LOG_TAG, "send Ticket for " + name + " " + ip);
+		if (!tickets.contains(ip)) {
+		    clients++;
+		    tickets.add(ip);
+		} else {
+		    Log.d(Constants.LOG_TAG, "reset sent ticket for " + name + " " + ip);
+		}
+	    } catch (Exception e) {
 		Log.e(Constants.LOG_TAG, "Exception in TicketManager", e);
-	  }
+	    }
 	} else {
-	  Log.d(Constants.LOG_TAG, "Server is full");
+	    Log.d(Constants.LOG_TAG, "Server is full");
 	}
 	// pech jehabt
-  }
+    }
 
-  public void reset() {
+    public void reset() {
 	tickets.clear();
-  }
+    }
 
-  public void connectionClosed(ClientSocket clientSocket) {
-	System.out.println("TicketManager connectionClosed");
+    public void connectionClosed(ClientSocket clientSocket) {
+	String ip = clientSocket.getDestinationAddress().getHostAddress();
+	System.out.println("TicketManager connectionClosed " + ip);
 	clients--;
-  }
+	tickets.remove(ip);
+    }
 
-  public void connectionOpened(ClientSocket clientSocket) {
+    public void connectionOpened(ClientSocket clientSocket) {
 	// TODO clients merken und abhaken
 
-  }
+    }
 
-  public int getMaxClients() {
+    public int getMaxClients() {
 	return maxClients;
-  }
+    }
 
-  public void setMaxClients(int maxClients) {
+    public void setMaxClients(int maxClients) {
 	this.maxClients = maxClients;
-  }
+    }
 }
