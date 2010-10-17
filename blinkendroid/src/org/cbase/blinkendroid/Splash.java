0cffbf3be5906b23be69a194cc3313449029656a
lischke <lischke@ded441ff-28c8-dbd9-35dd-4b5d9abe79e0>
2010-10-17 17:23:02 +0000

    removed overrides for interface implementations
    git-svn-id: http://blinkendroid.googlecode.com/svn/branches/development@497 ded441ff-28c8-dbd9-35dd-4b5d9abe79e0

--- blinkendroid/src/org/cbase/blinkendroid/Splash.java
+++ blinkendroid/src/org/cbase/blinkendroid/Splash.java
@@ -6,24 +6,24 @@ import android.os.Handler;
 
 public class Splash extends Activity {
 
-  @Override
-  public void onResume() {
+    @Override
+    public void onResume() {
 	super.onResume();
 	setContentView(R.layout.splashscreen_content);
 
 	new Handler().postDelayed(new Runnable() {
-	  @Override
-	  public void run() {
+
+	    public void run() {
 		try {
-		  Thread.sleep(1500);
+		    Thread.sleep(1500);
 		} catch (InterruptedException e) {
 		}
 		try {
-		  startActivity(new Intent(Splash.this, LoginActivity.class));
+		    startActivity(new Intent(Splash.this, LoginActivity.class));
 		} finally {
-		  finish();
+		    finish();
 		}
-	  }
+	    }
 	}, 2000);
-  }
+    }
 }
