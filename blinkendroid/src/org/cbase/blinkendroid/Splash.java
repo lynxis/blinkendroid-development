b3a19e451bf0c48c041c35015dcd7c903fdb81fd
benjamin.r.m.weiss@gmail.com <benjamin.r.m.weiss@gmail.com@ded441ff-28c8-dbd9-35dd-4b5d9abe79e0>
2010-10-17 15:49:20 +0000

    Added a splashscreen
    git-svn-id: http://blinkendroid.googlecode.com/svn/branches/development@494 ded441ff-28c8-dbd9-35dd-4b5d9abe79e0

---
+++ blinkendroid/src/org/cbase/blinkendroid/Splash.java
@@ -0,0 +1,29 @@
+package org.cbase.blinkendroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+
+public class Splash extends Activity {
+
+  @Override
+  public void onResume() {
+	super.onResume();
+	setContentView(R.layout.splashscreen_content);
+
+	new Handler().postDelayed(new Runnable() {
+	  @Override
+	  public void run() {
+		try {
+		  Thread.sleep(2000);
+		} catch (InterruptedException e) {
+		}
+		try {
+		  startActivity(new Intent(Splash.this, LoginActivity.class));
+		} finally {
+		  finish();
+		}
+	  }
+	}, 2000);
+  }
+}
