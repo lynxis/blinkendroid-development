68df811678f6d8a37c27a8532bc2a3897b668e55
benjamin.r.m.weiss@gmail.com <benjamin.r.m.weiss@gmail.com@ded441ff-28c8-dbd9-35dd-4b5d9abe79e0>
2010-10-17 15:52:25 +0000

    reduced splashscreen duration
    git-svn-id: http://blinkendroid.googlecode.com/svn/branches/development@495 ded441ff-28c8-dbd9-35dd-4b5d9abe79e0

--- blinkendroid/src/org/cbase/blinkendroid/Splash.java
+++ blinkendroid/src/org/cbase/blinkendroid/Splash.java
@@ -15,7 +15,7 @@ public class Splash extends Activity {
 	  @Override
 	  public void run() {
 		try {
-		  Thread.sleep(2000);
+		  Thread.sleep(1500);
 		} catch (InterruptedException e) {
 		}
 		try {
