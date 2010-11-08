093988982239a35609ea58d5af6c5ac7d28cd342
benjamin.r.m.weiss@gmail.com <benjamin.r.m.weiss@gmail.com@ded441ff-28c8-dbd9-35dd-4b5d9abe79e0>
2010-11-08 18:06:08 +0000

    GalaxyTab is now shown with landscape orientation
    git-svn-id: http://blinkendroid.googlecode.com/svn/branches/development@660 ded441ff-28c8-dbd9-35dd-4b5d9abe79e0

--- blinkendroid/src/org/cbase/blinkendroid/player/PlayerView.java
+++ blinkendroid/src/org/cbase/blinkendroid/player/PlayerView.java
@@ -20,6 +20,7 @@ package org.cbase.blinkendroid.player;
 import org.cbase.blinkendroid.player.bml.BLM;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -43,140 +44,153 @@ public class PlayerView extends ClippableView implements Runnable, BlinkenView {
     private final Handler handler = new Handler();
     private final Paint paint = new Paint();
 
+    private int screenLayout;
+
     private static final int PIXEL_PADDING = 1;
 
     public PlayerView(Context context, AttributeSet attrs) {
-	super(context, attrs);
+        super(context, attrs);
+        screenLayout = context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK;
+        System.out.println("ScreenLayout is : "+ screenLayout);
     }
 
     public void setBLM(final BLM blm) {
 
-	// trying to free some memory first
-	System.gc();
+        // trying to free some memory first
+        System.gc();
 
-	this.blm = blm;
-	this.numFrames = blm.frames.size();
-	long time = 0;
-	frameTime = new long[numFrames + 1];
-	for (int i = 0; i < numFrames; i++) {
-	    frameTime[i] = time;
-	    time += blm.frames.get(i).duration;
-	}
-	frameTime[numFrames] = time;
-	duration = time;
-	frame = 0;
+        this.blm = blm;
+        this.numFrames = blm.frames.size();
+        long time = 0;
+        frameTime = new long[numFrames + 1];
+        for (int i = 0; i < numFrames; i++) {
+            frameTime[i] = time;
+            time += blm.frames.get(i).duration;
+        }
+        frameTime[numFrames] = time;
+        duration = time;
+        frame = 0;
     }
 
     public void setStartTime(long startTime) {
-	this.startTime = startTime;
+        this.startTime = startTime;
     }
 
     public void setTimeDelta(long timeDelta) {
-	this.timeDelta = timeDelta;
+        this.timeDelta = timeDelta;
     }
 
     public void startPlaying() {
-	if (!playing) {
-	    playing = true;
-	    handler.post(this);
-	}
+        if (!playing) {
+            playing = true;
+            handler.post(this);
+        }
     }
 
     public void stopPlaying() {
-	if (playing) {
-	    playing = false;
-	    handler.removeCallbacks(this);
-	}
+        if (playing) {
+            playing = false;
+            handler.removeCallbacks(this);
+        }
     }
 
     @Override
     protected void onDraw(final Canvas canvas) {
 
-	if (blm != null) {
+        if (blm != null) {
 
-	    final byte[][] matrix = blm.frames.get(frame).matrix;
+            final byte[][] matrix = blm.frames.get(frame).matrix;
 
-	    final int absStartX = (int) (blm.header.width * startX);
-	    final int absStartY = (int) (blm.header.height * startY);
-	    final int absEndX = (int) (blm.header.width * endX);
-	    final int absEndY = (int) (blm.header.height * endY);
+            final int absStartX = (int) (blm.header.width * startX);
+            final int absStartY = (int) (blm.header.height * startY);
+            final int absEndX = (int) (blm.header.width * endX);
+            final int absEndY = (int) (blm.header.height * endY);
 
-	    final float pixelWidth = (float) getWidth() / (absEndX - absStartX);
-	    final float pixelHeight = (float) getHeight() / (absEndY - absStartY);
+            final float pixelWidth = (float) getWidth() / (absEndX - absStartX);
+            final float pixelHeight = (float) getHeight() / (absEndY - absStartY);
 
-	    // clip
-	    int red = 0;
-	    int green = 0;
-	    int blue = 0;
-	    for (int y = absStartY; y < absEndY; y++) {
-		final int clippedY = y - absStartY;
-		final byte[] row = matrix[y];
-		for (int x = absStartX; x < absEndX; x++) {
-		    final int clippedX = x - absStartX;
-		    final int value = row[x] << (8 - blm.header.bits);
-		    if (blm.header.color) {
-			if (blm.header.bits == 6) {
-			    red = ((row[x] & 48) >> 4) * 64;
-			    green = ((row[x] & 12) >> 2) * 64;
-			    blue = (row[x] & 3) * 64;
-			} else if (blm.header.bits == 8) {
-			    red = ((row[x] & 224) >> 5) * 32;
-			    green = ((row[x] & 28) >> 2) * 32;
-			    blue = (row[x] & 3) * 64;
-			}
-			// Log.d(Constants.LOG_TAG, r+","+g+","+b+":"+
-			// row[x]+";");
-			paint.setColor(Color.argb(255, blink ? 255 - red : red, blink ? 255 - green : green,
-				blink ? 255 - blue : blue));
-		    } else {
-			paint.setColor(Color.argb(255, blink ? 255 - value : value, blink ? 255 - value : value,
-				blink ? 255 - value : value));
-		    }
-		    canvas.drawRect(pixelWidth * clippedX + PIXEL_PADDING, pixelHeight * clippedY + PIXEL_PADDING,
-			    pixelWidth * (clippedX + 1) - PIXEL_PADDING, pixelHeight * (clippedY + 1) - PIXEL_PADDING,
-			    paint);
-		}
-	    }
-	}
+            // clip
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+            for (int y = absStartY; y < absEndY; y++) {
+                final int clippedY = y - absStartY;
+                final byte[] row = matrix[y];
+                for (int x = absStartX; x < absEndX; x++) {
+                    final int clippedX = x - absStartX;
+                    final int value = row[x] << (8 - blm.header.bits);
+                    if (blm.header.color) {
+                        if (blm.header.bits == 6) {
+                            red = ((row[x] & 48) >> 4) * 64;
+                            green = ((row[x] & 12) >> 2) * 64;
+                            blue = (row[x] & 3) * 64;
+                        } else if (blm.header.bits == 8) {
+                            red = ((row[x] & 224) >> 5) * 32;
+                            green = ((row[x] & 28) >> 2) * 32;
+                            blue = (row[x] & 3) * 64;
+                        }
+                        // Log.d(Constants.LOG_TAG, r+","+g+","+b+":"+
+                        // row[x]+";");
+                        paint.setColor(Color.argb(255, blink ? 255 - red : red, blink ? 255 - green : green,
+                                blink ? 255 - blue : blue));
+                    } else {
+                        paint.setColor(Color.argb(255, blink ? 255 - value : value, blink ? 255 - value : value,
+                                blink ? 255 - value : value));
+                    }
+                    //GalaxyTab
+                    if (Configuration.SCREENLAYOUT_SIZE_LARGE == screenLayout) {
+                        canvas.drawRect(pixelWidth * clippedY + PIXEL_PADDING, pixelHeight * clippedX + PIXEL_PADDING,
+                                pixelWidth * (clippedY + 1) - PIXEL_PADDING, pixelHeight * (clippedX + 1)
+                                        - PIXEL_PADDING, paint);
+                    } else {
+                        //other devices
+                        canvas.drawRect(pixelWidth * clippedX + PIXEL_PADDING, pixelHeight * clippedY + PIXEL_PADDING,
+                                pixelWidth * (clippedX + 1) - PIXEL_PADDING, pixelHeight * (clippedY + 1)
+                                        - PIXEL_PADDING, paint);
+                        
+                    }
+                }
+            }
+        }
     }
 
     public void run() {
 
-	// time into movie, taking endless looping into account
-	final long serverTime = System.nanoTime() / 1000000 - timeDelta;
-	long time = (serverTime - startTime) % duration;
-	if (time < 0)
-	    time = duration + time;
+        // time into movie, taking endless looping into account
+        final long serverTime = System.nanoTime() / 1000000 - timeDelta;
+        long time = (serverTime - startTime) % duration;
+        if (time < 0)
+            time = duration + time;
 
-	// determine frame to be displayed
-	long nextFrameTime;
-	while (true) {
-	    if (time < frameTime[frame]) {
-		frame--;
-		continue;
-	    }
-	    nextFrameTime = frameTime[frame + 1];
-	    if (time >= nextFrameTime) {
-		frame++;
-		continue;
-	    }
-	    break;
-	}
+        // determine frame to be displayed
+        long nextFrameTime;
+        while (true) {
+            if (time < frameTime[frame]) {
+                frame--;
+                continue;
+            }
+            nextFrameTime = frameTime[frame + 1];
+            if (time >= nextFrameTime) {
+                frame++;
+                continue;
+            }
+            break;
+        }
 
-	// display frame asap
-	invalidate();
+        // display frame asap
+        invalidate();
 
-	// wait until next frame
-	handler.postDelayed(this, nextFrameTime - time);
+        // wait until next frame
+        handler.postDelayed(this, nextFrameTime - time);
     }
 
     public void blink(int type) {
-	// logger.info("blink " + type);
-	blink = true;
-	handler.postDelayed(new Runnable() {
-	    public void run() {
-		blink = false;
-	    }
-	}, 500);
+        // logger.info("blink " + type);
+        blink = true;
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                blink = false;
+            }
+        }, 500);
     }
 }
